dd128f1fb7013661991a8aa99444a012b3e674cd
Tetsuto <htetsu0227@gmail.com>
2026-02-19 16:53:31 +0900

    feat(api): add easy api for infra test

---
+++ backend/src/ts/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import { type Request, type Response } from 'express';
+
+
+const app = express();
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).type('text/plain').send('Hello World!');
+});
+
+const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Listening on ${port}`);
+})
